39c7b2b895fcdecb2282d99b60902996107ff286
penguin <asimbaidya.me@gmail.com>
2023-10-30 09:56:32 +0600

    add module
    1. admin login
    2. provider login
    3. provider registation
    4. models for provider and admin

--- glamsage/models.py
+++ glamsage/models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    profile_image = db.Column(db.String(60), nullable=True, default="default.jpg")
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
@@ -15,6 +16,22 @@ class User(db.Model):
 
 class Provider(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    brand_name = db.Column(db.String(20), nullable=False)
+    brand_title = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    brand_profile_image = db.Column(
+        db.String(60), nullable=True, default="default-brand-profile.jpg"
+    )
+    brand_cover_image = db.Column(
+        db.String(60), nullable=True, default="default-brand-cover.jpg"
+    )
+
+
+# to manage users and providers
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    profile_image = db.Column(db.String(60), nullable=True, default="default.jpg")
